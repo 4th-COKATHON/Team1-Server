13eb1b60c0c37b579945b13a468d1dff35533bae
youth <boysoeng@naver.com>
2024-07-26 17:21:44 +0900

    feat: test api 세팅

--- src/main/java/cotato/hackathon/team1/controller/TestController.java
+++ src/main/java/cotato/hackathon/team1/controller/TestController.java
@@ -1,6 +1,7 @@
 package cotato.hackathon.team1.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/test")
 public class TestController {
 
+    @GetMapping
     public ResponseEntity<String> test() {
         return ResponseEntity.ok().body("ok");
     }
